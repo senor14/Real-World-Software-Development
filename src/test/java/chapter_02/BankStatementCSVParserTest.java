56715de62ac07c30b7362e40a0b07b2507277d03
senor14 <dkssud940@gmail.com>
2023-05-25 11:10:38 +0900

    test: CSVParser test

---
+++ src/test/java/chapter_02/BankStatementCSVParserTest.java
@@ -0,0 +1,28 @@
+package chapter_02;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() {
+        final String line = "30-01-2017,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected
+                = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+}
